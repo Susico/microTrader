00dd016602d697816b8ab547b72f9a1c8096d141
Suminda Dharmasena <sirinath1978m@gmail.com>
2014-06-19 22:18:47 +0530

    Removed Logging

--- Disruptor/src/main/groovy/com/susico/disruptor/events/factories/ExecutionReportEvent.groovy
+++ Disruptor/src/main/groovy/com/susico/disruptor/events/factories/ExecutionReportEvent.groovy
@@ -2,15 +2,13 @@ package com.susico.disruptor.events.factories
 
 import com.lmax.disruptor.EventFactory
 import com.susico.event.ExecutionReport
-import groovy.transform.AutoClone
-import groovy.transform.AutoExternalize
 import groovy.transform.Canonical
 import groovy.transform.CompileStatic
 
 /**
  * Created by Suminda on 18/06/2014.
  */
-@CompileStatic @AutoExternalize @AutoClone @Canonical
+@CompileStatic @Canonical
 class ExecutionReportEvent extends ExecutionReport
       implements EventFactory<ExecutionReportEvent> {
    @Override
